175ac1ed59f3851f85431297287e88a5a966ead3
mikhaildadaev <mikhaildadaev@gmail.com>
2026-05-01 20:28:35 +0300

    Обновлени бенчмарков

--- uuid_bench_test.go
+++ uuid_bench_test.go
@@ -23,43 +23,6 @@ func Benchmark_NewV1(b *testing.B) {
 		}
 	})
 }
-func Benchmark_NewV1_Info(b *testing.B) {
-	b.Run("With Mock Time", func(b *testing.B) {
-		// Ленивая инициализация глобального состояния
-		initSync.Do(func() {
-			initError = initGlobal()
-		})
-		if initError != nil {
-			b.Fatal(initError)
-		}
-		// Фиксация текущего состояние
-		realClock := initClock
-		realTime := v1.lastTime.Load()
-		realSequence := v1.lastSequence.Load()
-		b.Cleanup(func() {
-			initClock = realClock
-			v1.lastTime.Store(realTime)
-			v1.lastSequence.Store(realSequence)
-		})
-		// Мокирование
-		clockMock := &clockMock{time: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)}
-		initClock = clockMock
-		v1.lastTime.Store(uint64((initClock.now().UnixNano() / 100) + offsetTime))
-		v1.lastSequence.Store(0)
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV1()
-			b.Logf("UUIDv1-%d: %s", i, ui)
-		}
-	})
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV1()
-			b.Logf("UUIDv1-%d: %s", i, ui)
-		}
-	})
-}
 func Benchmark_NewV2(b *testing.B) {
 	b.Run("Multi", func(b *testing.B) {
 		b.ResetTimer()
@@ -73,15 +36,6 @@ func Benchmark_NewV2(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			_ = NewV2(testPOSType, testPOSValue)
-		}
-	})
-}
-func Benchmark_NewV2_Info(b *testing.B) {
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV2(testPOSType, testPOSValue)
-			b.Logf("UUIDv2-%d: %s", i, ui)
 		}
 	})
 }
@@ -101,15 +55,6 @@ func Benchmark_NewV3(b *testing.B) {
 		}
 	})
 }
-func Benchmark_NewV3_Info(b *testing.B) {
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV3(NameSpaceDNS, testNameString+strconv.Itoa(i))
-			b.Logf("UUIDv3-%d: %s", i, ui)
-		}
-	})
-}
 func Benchmark_NewV4(b *testing.B) {
 	b.Run("Multi", func(b *testing.B) {
 		b.ResetTimer()
@@ -123,15 +68,6 @@ func Benchmark_NewV4(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			_ = NewV4()
-		}
-	})
-}
-func Benchmark_NewV4_Info(b *testing.B) {
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV4()
-			b.Logf("UUIDv4-%d: %s", i, ui)
 		}
 	})
 }
@@ -151,15 +87,6 @@ func Benchmark_NewV5(b *testing.B) {
 		}
 	})
 }
-func Benchmark_NewV5_Info(b *testing.B) {
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV5(NameSpaceDNS, testNameString+strconv.Itoa(i))
-			b.Logf("UUIDv5-%d: %s", i, ui)
-		}
-	})
-}
 func Benchmark_NewV6(b *testing.B) {
 	b.Run("Multi", func(b *testing.B) {
 		b.ResetTimer()
@@ -173,43 +100,6 @@ func Benchmark_NewV6(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			_ = NewV6()
-		}
-	})
-}
-func Benchmark_NewV6_Info(b *testing.B) {
-	b.Run("With Mock Time", func(b *testing.B) {
-		// Ленивая инициализация глобального состояния
-		initSync.Do(func() {
-			initError = initGlobal()
-		})
-		if initError != nil {
-			b.Fatal(initError)
-		}
-		// Фиксация текущего состояние
-		realClock := initClock
-		realTime := v6.lastTime.Load()
-		realSequence := v6.lastSequence.Load()
-		b.Cleanup(func() {
-			initClock = realClock
-			v6.lastTime.Store(realTime)
-			v6.lastSequence.Store(realSequence)
-		})
-		// Мокирование
-		clockMock := &clockMock{time: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)}
-		initClock = clockMock
-		v6.lastTime.Store(uint64((initClock.now().UnixNano() / 100) + offsetTime))
-		v6.lastSequence.Store(0)
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV6()
-			b.Logf("UUIDv6-%d: %s", i, ui)
-		}
-	})
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV6()
-			b.Logf("UUIDv6-%d: %s", i, ui)
 		}
 	})
 }
@@ -229,43 +119,6 @@ func Benchmark_NewV7(b *testing.B) {
 		}
 	})
 }
-func Benchmark_NewV7_Info(b *testing.B) {
-	b.Run("With Mock Time", func(b *testing.B) {
-		// Ленивая инициализация глобального состояния
-		initSync.Do(func() {
-			initError = initGlobal()
-		})
-		if initError != nil {
-			b.Fatal(initError)
-		}
-		// Фиксация текущего состояние
-		realClock := initClock
-		realTime := v7.lastTime.Load()
-		realSequence := v7.lastSequence.Load()
-		b.Cleanup(func() {
-			initClock = realClock
-			v7.lastTime.Store(realTime)
-			v7.lastSequence.Store(realSequence)
-		})
-		// Мокирование
-		clockMock := &clockMock{time: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)}
-		initClock = clockMock
-		v7.lastTime.Store(uint64(initClock.now().UnixMicro()))
-		v7.lastSequence.Store(0)
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV7()
-			b.Logf("UUIDv7-%d: %s", i, ui)
-		}
-	})
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV7()
-			b.Logf("UUIDv7-%d: %s", i, ui)
-		}
-	})
-}
 func Benchmark_NewV8(b *testing.B) {
 	b.Run("Multi", func(b *testing.B) {
 		b.ResetTimer()
@@ -279,43 +132,6 @@ func Benchmark_NewV8(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			_ = NewV8(testNodeID)
-		}
-	})
-}
-func Benchmark_NewV8_Info(b *testing.B) {
-	b.Run("With Mock Time", func(b *testing.B) {
-		// Ленивая инициализация глобального состояния
-		initSync.Do(func() {
-			initError = initGlobal()
-		})
-		if initError != nil {
-			b.Fatal(initError)
-		}
-		// Фиксация текущего состояние
-		realClock := initClock
-		realTime := v8.lastTime.Load()
-		realSequence := v8.lastSequence.Load()
-		b.Cleanup(func() {
-			initClock = realClock
-			v8.lastTime.Store(realTime)
-			v8.lastSequence.Store(realSequence)
-		})
-		// Мокирование
-		clockMock := &clockMock{time: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)}
-		initClock = clockMock
-		v8.lastTime.Store(uint64(initClock.now().UnixMicro()))
-		v8.lastSequence.Store(0)
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV8(testNodeID)
-			b.Logf("UUIDv8-%d: %s", i, ui)
-		}
-	})
-	b.Run("Without Mock Time", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			ui := NewV8(testNodeID)
-			b.Logf("UUIDv8-%d: %s", i, ui)
 		}
 	})
 }
